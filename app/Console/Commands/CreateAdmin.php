1a55818f80a9c2b5f39105f771ad4702d8b2bd08
monikasmilga <tuuli705@gmail.com>
2017-06-07 22:55:33 +0300

    Created artisan command to create admin through command line

---
+++ app/Console/Commands/CreateAdmin.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\VRUsers;
+use Illuminate\Console\Command;
+use Ramsey\Uuid\Uuid;
+
+class CreateAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:admin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates admin user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->comment('Creating admin user');
+        $name = $this->ask('insert name');
+        $email = $this->ask('insert email');
+        $password = $this->secret('insert password');
+        $phone = $this->ask('insert phone');
+
+        $record = VRUsers::create([
+            'id' => Uuid::uuid4(),
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt($password),
+            'phone' => $phone,
+
+        ]);
+        $record->role()->sync('super-admin');
+
+    }
+}
